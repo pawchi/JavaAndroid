c327f4384c84a0a801bcecc88189ada9f99c960f
pawchi <pawchi@o2.pl>
2019-07-10 15:11:20 +0200

    Emergency_notifier continue_3 send SMS

--- EmergencyNotifier/app/src/main/java/org/chilon/emergencynotifier/Sms.java
+++ EmergencyNotifier/app/src/main/java/org/chilon/emergencynotifier/Sms.java
@@ -3,7 +3,6 @@ package org.chilon.emergencynotifier;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -27,10 +26,10 @@ public class Sms extends AppCompatActivity {
 
         number = findViewById(R.id.input_phone_number_field);
         message = findViewById(R.id.sms_input_text_field);
-        send = findViewById(R.id.smsButton);
+        send = findViewById(R.id.sms_send_button);
 
         send.setEnabled(false);
-        if (checkPermission(Manifest.permission.SEND_SMS)){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
             send.setEnabled(true);
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQUEST_CODE);
@@ -46,18 +45,14 @@ public class Sms extends AppCompatActivity {
             return ;
         }
 
-        if (checkPermission(Manifest.permission.SEND_SMS)){
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, smsMessage, null,null);
-            Toast.makeText(this, "Message Sent!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Message Sent!", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
         }
 
-    }
-
-    public boolean checkPermission(String permission){
-        int check = ContextCompat.checkSelfPermission(this, permission);
-        return (check == PackageManager.PERMISSION_GRANTED);
     }
 }
